20fa5e003bf15bf38b33b28d8b5c37bc306a4d1f
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-09-05 13:44:41 -0300

    initial code

---
+++ src/app/components/ListItems copy.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import ItemJSON from '../Items/ItemJSON';
+export default class ListItems extends React.Component {
+	
+	// This code is meant for deletion
+	deleteProduct(e) {
+		e.preventDefault();
+		ItemJSON.deleteProducts(this.props.product.productId);
+		ItemJSON.emitChange();
+	}
+
+	render() {
+		const product = this.props.product;
+
+		return (
+			// displaying available products and it
+			// is having delete action
+			<li>
+				<table border="3">
+					<tbody>
+						<tr>
+							<td width="100px" id="one">
+                                <span className='image'>
+                               <img src={product.product_imageURl} ></img>
+                                </span>
+                                </td>
+                            <td width="100px" id="two">
+								<span  >
+                                {product.productName} </span>
+							</td>
+							<td width="100px" id='Three'>
+                                <span>
+								{product.productPrice}</span>
+							</td>
+							<td width="100px">
+                                <span>
+								{product.productQuantity}</span>
+							</td>
+							<td width="100px"><button
+								className="delete"
+								onClick={this.deleteProduct.bind(this)}>
+								Delete </button>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</li>
+		);
+	}
+}
